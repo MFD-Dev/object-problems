6b8702cf8821b39ab0f12e3ef90c68ca05982103
Michael <Michaelflowersdev@gmail.com>
2021-07-27 21:49:10 -0500

    added comment notes

--- object-problem4.js
+++ object-problem4.js
@@ -2,7 +2,14 @@
 Objects: Problem 4
 Module 3: JavaScript
 Create an object, then write a JavaScript function that checks 
-whether an object contains the specified key.
+whether an object contains the specific property.
+*/
+/*
+Four ways to check if a Object has a property:
+1. hasOwnProperty
+2. in Operator
+3. using typeOf and compare it with undefined.
+4. using !! operator(double-bang operator)
 */
 
 
